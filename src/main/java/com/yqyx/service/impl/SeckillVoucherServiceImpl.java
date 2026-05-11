6d91a13c5176ba817856969fbb9e7f5f95d9b6e5
zhaojun <111044543+ZJunCher@users.noreply.github.com>
2026-05-11 14:46:33 +0800

    Add src/main/java/com/yqyx/service/impl/SeckillVoucherServiceImpl.java

---
+++ src/main/java/com/yqyx/service/impl/SeckillVoucherServiceImpl.java
@@ -0,0 +1,19 @@
+package com.yqyx.service.impl;
+
+import com.yqyx.entity.SeckillVoucher;
+import com.yqyx.mapper.SeckillVoucherMapper;
+import com.yqyx.service.ISeckillVoucherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 秒杀优惠券表，与优惠券是一对一关系 服务实现类
+ * </p>
+ *
+ * @since 2022-01-04
+ */
+@Service
+public class SeckillVoucherServiceImpl extends ServiceImpl<SeckillVoucherMapper, SeckillVoucher> implements ISeckillVoucherService {
+
+}
