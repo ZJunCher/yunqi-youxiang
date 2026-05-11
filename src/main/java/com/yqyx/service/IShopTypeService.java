a4ce0d9bfc1820f5ec5894d0b5365e1c85172bf8
zhaojun <111044543+ZJunCher@users.noreply.github.com>
2026-05-11 14:46:16 +0800

    Add src/main/java/com/yqyx/service/IShopTypeService.java

---
+++ src/main/java/com/yqyx/service/IShopTypeService.java
@@ -0,0 +1,15 @@
+package com.yqyx.service;
+
+import com.yqyx.entity.ShopType;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @since 2021-12-22
+ */
+public interface IShopTypeService extends IService<ShopType> {
+
+}
