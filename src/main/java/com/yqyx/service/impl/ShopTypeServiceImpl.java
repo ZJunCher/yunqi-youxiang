70cac26fec35125d6f3db1f1501be8ac98ca448a
zhaojun <111044543+ZJunCher@users.noreply.github.com>
2026-05-11 14:46:36 +0800

    Add src/main/java/com/yqyx/service/impl/ShopTypeServiceImpl.java

---
+++ src/main/java/com/yqyx/service/impl/ShopTypeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.yqyx.service.impl;
+
+import com.yqyx.entity.ShopType;
+import com.yqyx.mapper.ShopTypeMapper;
+import com.yqyx.service.IShopTypeService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @since 2021-12-22
+ */
+@Service
+public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
+
+}
