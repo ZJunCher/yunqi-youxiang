7c98f3d9a17f1e5520bd0d8b8eee6e3fbdbbfa3e
zhaojun <111044543+ZJunCher@users.noreply.github.com>
2026-05-11 14:46:11 +0800

    Add src/main/java/com/yqyx/service/IFollowService.java

---
+++ src/main/java/com/yqyx/service/IFollowService.java
@@ -0,0 +1,21 @@
+package com.yqyx.service;
+
+import com.yqyx.dto.Result;
+import com.yqyx.entity.Follow;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @since 2021-12-22
+ */
+public interface IFollowService extends IService<Follow> {
+
+    Result follow(Long followUserId, Boolean isFollow);
+
+    Result isFollow(Long followUserId);
+
+    Result followCommons(Long id);
+}
