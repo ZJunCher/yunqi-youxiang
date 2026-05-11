d61b36862e6308241b7f64071e5222abf55367bb
zhaojun <111044543+ZJunCher@users.noreply.github.com>
2026-05-11 14:46:23 +0800

    Add src/main/java/com/yqyx/service/IVoucherService.java

---
+++ src/main/java/com/yqyx/service/IVoucherService.java
@@ -0,0 +1,19 @@
+package com.yqyx.service;
+
+import com.yqyx.dto.Result;
+import com.yqyx.entity.Voucher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @since 2021-12-22
+ */
+public interface IVoucherService extends IService<Voucher> {
+
+    Result queryVoucherOfShop(Long shopId);
+
+    void addSeckillVoucher(Voucher voucher);
+}
