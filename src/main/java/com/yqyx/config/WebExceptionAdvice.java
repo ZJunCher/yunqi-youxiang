bda8bfeb67a5b09f76c1035ed033bc7f859bcd6a
zhaojun <111044543+ZJunCher@users.noreply.github.com>
2026-05-11 14:45:00 +0800

    Add src/main/java/com/yqyx/config/WebExceptionAdvice.java

---
+++ src/main/java/com/yqyx/config/WebExceptionAdvice.java
@@ -0,0 +1,23 @@
+package com.yqyx.config;
+
+import com.yqyx.dto.Result;
+import com.yqyx.limiter.exception.RateLimitException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class WebExceptionAdvice {
+
+    @ExceptionHandler(RateLimitException.class)
+    public Result handleRateLimitException(RateLimitException e) {
+        return Result.fail(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public Result handleRuntimeException(RuntimeException e) {
+        log.error(e.toString(), e);
+        return Result.fail("服务器异常");
+    }
+}
