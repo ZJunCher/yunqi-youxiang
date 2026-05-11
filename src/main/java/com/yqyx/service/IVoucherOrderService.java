d573d60a20b5e2fc6f61eb0a74f619cbc8ed1f63
zhaojun <111044543+ZJunCher@users.noreply.github.com>
2026-05-11 14:46:21 +0800

    Add src/main/java/com/yqyx/service/IVoucherOrderService.java

---
+++ src/main/java/com/yqyx/service/IVoucherOrderService.java
@@ -0,0 +1,23 @@
+package com.yqyx.service;
+
+import com.yqyx.dto.Result;
+import com.yqyx.entity.VoucherOrder;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @since 2021-12-22
+ */
+public interface IVoucherOrderService extends IService<VoucherOrder> {
+
+    Result seckillVoucher(Long voucherId);
+
+    Result queryOrderById(Long orderId);
+
+    Result queryMyOrders(Integer current);
+
+    Result handlePaySuccess(Long orderId);
+}
