847ee0481c25e61be2a4eeb2a6c3849954bf808f
zhaojun <111044543+ZJunCher@users.noreply.github.com>
2026-05-11 14:46:07 +0800

    Add src/main/java/com/yqyx/service/IBlogCommentsService.java

---
+++ src/main/java/com/yqyx/service/IBlogCommentsService.java
@@ -0,0 +1,15 @@
+package com.yqyx.service;
+
+import com.yqyx.entity.BlogComments;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @since 2021-12-22
+ */
+public interface IBlogCommentsService extends IService<BlogComments> {
+
+}
