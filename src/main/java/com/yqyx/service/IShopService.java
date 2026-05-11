0123b4e689f532e6a57323da14ea6bab6b870640
zhaojun <111044543+ZJunCher@users.noreply.github.com>
2026-05-11 14:46:14 +0800

    Add src/main/java/com/yqyx/service/IShopService.java

---
+++ src/main/java/com/yqyx/service/IShopService.java
@@ -0,0 +1,23 @@
+package com.yqyx.service;
+
+import com.yqyx.dto.Result;
+import com.yqyx.entity.Shop;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @since 2021-12-22
+ */
+public interface IShopService extends IService<Shop> {
+
+    Result queryById(Long id);
+
+    Result update(Shop shop);
+
+    Result queryShopByType(Integer typeId, Integer current, Double x, Double y);
+
+    Result saveShopToRedis(Long id, Long expireSeconds);
+}
